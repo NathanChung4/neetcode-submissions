179490a3a05bf3252c6a4701e445134b8e7c3ec5
Nathan Chung <111595525+NathanChung4@users.noreply.github.com>
2026-08-14 12:25:46 -0500

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> res(nums.size());
+        int prefix = 1;
+        int postfix = 1;
+
+        // first pass is prefix
+        for (int i = 0; i < nums.size(); i++) {
+            res[i] = prefix;
+            prefix = prefix * nums[i];
+        }
+        // second pass is apply postfix
+        for (int i = nums.size()-1; i >= 0; i--) {
+            res[i] = res[i] * postfix;
+            postfix = postfix * nums[i];
+        }
+        return res;
+
+
+    }
+};
